783beba0b5e2b32d84f9c6a9be166640545b55dc
rsd1987 <rsd1987@qq.com>
2018-07-20 20:50:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
 sed -i "s/CLIENT_ALTERID/$CLIENT_ALTERID/g" /etc/v2ray/config.json
 sed -i "s#CLIENT_WSPATH#$CLIENT_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#CLIENT_WSPATH#$CLIENT_WSPATH#g" /etc/nginx/conf.d/default.conf
+v2ray -config=/etc/v2ray/config.json &
 nginx -g 'daemon off;'
-v2ray -config=/etc/v2ray/config.json
